a9863e828a64664dfd71a56417983cc4c5f0c396
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-04-20 15:46:17 +1000

    Moved some stuff around and made it a vector

--- lua/autorun/client/spark2D.lua
+++ lua/autorun/client/spark2D.lua
@@ -11,12 +11,11 @@ local function randomFloat( min, max )
 	return min + math.random()  * ( max - min )
 end
 
-local function CreateSparks( x, y )
+local function CreateSparks( pos )
 	for i = 0, math.random( minparticles, maxparticles ) do
 		table.insert( sparks, {
-			x = x, y = y,
-			xvel = randomFloat( -speed, speed ),
-			yvel = randomFloat( -speed, speed ),
+			pos = pos,
+			vel = Vector( randomFloat( -speed, speed ), randomFloat( -speed, speed ) ),
 			size = randomFloat( minsize, maxsize ),
 			col = Color( 255, 255, 0 )
 		} )
@@ -27,28 +26,23 @@ local function DrawSparks( w, h )
 
 	for k, sp in ipairs( sparks ) do
 
-		local rnd = math.random() * colorshakiness
-
-		surface.SetDrawColor( sp.col.r + rnd, sp.col.g + rnd, sp.col.b + rnd )
-		surface.DrawRect( sp.x - sp.size / 2, sp.y - sp.size / 2, sp.size, sp.size )
-
-		sp.x = sp.x + sp.xvel
-		sp.y = sp.y + sp.yvel
-
-		if bounceoffedges then
-			if sp.x <= 0 or sp.x >= w then sp.xvel = -sp.xvel end
-			if sp.y <= 0 or sp.y >= h then sp.yvel = -sp.yvel end
-		end
-
-		sp.xvel = sp.xvel + randomFloat( -posshakiness, posshakiness )
-		sp.yvel = sp.yvel + randomFloat( -posshakiness, posshakiness )
-
+		sp.vel = sp.vel + Vector( randomFloat( -posshakiness, posshakiness ), randomFloat( -posshakiness, posshakiness ) )
 		sp.col.g = sp.col.g - math.random() * colordecay
+		sp.pos = sp.pos + sp.vel
 
 		sp.size = sp.size / sizedecay
 		if sp.size < removesize then
 			table.remove( sparks, k )
 		end
+
+		if bounceoffedges then
+			if sp.pos.x <= 0 or sp.pos.x >= w then sp.vel.x = -sp.vel.x end
+			if sp.pos.y <= 0 or sp.pos.y >= h then sp.vel.x = -sp.vel.y end
+		end
+
+		local rnd = math.random() * colorshakiness
+		surface.SetDrawColor( sp.col.r + rnd, sp.col.g + rnd, sp.col.b + rnd )
+		surface.DrawRect( sp.pos.x - sp.size / 2, sp.pos.y - sp.size / 2, sp.size, sp.size )
 
 	end
 
@@ -65,20 +59,14 @@ concommand.Add( "SparkTest", function()
 	local Panel = vgui.Create( "Panel", Frame )
 	Panel:Dock( FILL )
 
+	function Panel:OnMousePressed() self.Depressed = true end
+	function Panel:OnMouseReleased() self.Depressed = false end
+	
 	function Panel:Paint( w, h )
-
-		if self.Depressed then
-			CreateSparks( self:CursorPos() )
-		end
-
+		if self.Depressed then CreateSparks( Vector( self:CursorPos() ) ) end
 		surface.SetDrawColor( 0, 0, 0 )
 		surface.DrawRect( 0, 0, w, h )
 		DrawSparks( w, h )
-
 	end
-
-	function Panel:OnMousePressed() self.Depressed = true end
-
-	function Panel:OnMouseReleased() self.Depressed = false end
 
 end )
